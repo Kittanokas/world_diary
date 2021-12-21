a2c97302f2720d67998f0b8ae55513d12a295fcc
Kittanokas <kitai1012@gmail.com>
2021-12-21 15:20:38 +0900

    ActiveHashの導入

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,8 +1,11 @@
 class Diary < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :country
   belongs_to :user
   has_one_attached :image
 
   validates :content, presence: true
   validates :city, presence: true
   validates :title, presence: true
+  validates :country_id, numericality: { other_than: 1 ,message: "can't be blank" } 
 end
